f957b452f5f557db94c857f92bcebe05f7decc1d
Simara Formiga <simara20simara@gmail.com>
2023-12-10 11:20:45 -0300

    Esqueci de tirar um comentário

--- src/br/com/projeto/prova/Contratos/IDao.java
+++ src/br/com/projeto/prova/Contratos/IDao.java
@@ -3,8 +3,6 @@ package br.com.projeto.prova.Contratos;
 import java.util.ArrayList;
 
 public interface IDao<T> {
-    //Create, Read, Update, Delete
-    //Criar, buscar, Atualizar, Excluir, Listar
     void adicionar(T objeto);
     T buscar(int id);
     ArrayList<T> listarTodos();
